d490ccce32b83ea79818a656482c64677480de34
Lucas Dallavalle <89872652+caaslu@users.noreply.github.com>
2025-07-31 13:29:37 -0300

    Create campanhas.js

---
+++ api/campanhas.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const scriptURL = "https://script.google.com/macros/s/AKfycbzrAqRel710HArCvHfLt_Tupwf3UQqIX79QBrJkUjnWesIk01wkdI-H7gOnzAJ3uoQ1SQ/exec";
+
+  try {
+    const response = await fetch(scriptURL);
+    const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: "Erro ao buscar dados: " + error.message });
+  }
+}
