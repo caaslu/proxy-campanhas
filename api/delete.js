0839482a5d40e9893c834620ff09af137fd1fe58
Lucas Dallavalle <89872652+caaslu@users.noreply.github.com>
2025-07-31 14:26:10 -0300

    Update delete.js

--- api/delete.js
+++ api/delete.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // <-- aqui também
     return res.status(405).json({ error: "Method not allowed" });
   }
 
@@ -15,7 +16,9 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.setHeader("Access-Control-Allow-Origin", "*");
     return res.status(200).json(data);
+
   } catch (error) {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // <-- adicionar aqui também
     return res.status(500).json({ error: "Erro ao excluir: " + error.message });
   }
 }
